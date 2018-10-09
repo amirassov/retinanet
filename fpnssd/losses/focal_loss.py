23111366d9d7bd73e00723da988587c7d418d90e
Vsevolod Poletaev <poletaev.va@gmail.com>
2018-10-09 19:26:19 +0300

    PyTorch 0.4.1 warnings fixed

--- fpnssd/losses/focal_loss.py
+++ fpnssd/losses/focal_loss.py
@@ -1,11 +1,10 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class FocalLoss(nn.Module):
     def __init__(self, alpha=1, gamma=2):
         super().__init__()
-        self.nll = nn.NLLLoss(size_average=False)
+        self.nll = nn.NLLLoss(reduction='sum')
         self.alpha = alpha
         self.gamma = gamma
 
@@ -20,7 +19,7 @@ class FocalLoss(nn.Module):
         num_positive = positive.sum().item()
 
         mask = positive.unsqueeze(2).expand_as(bbox_input)
-        bbox_loss = F.smooth_l1_loss(bbox_input[mask], bbox_target[mask], size_average=False)
-        label_loss = F.nll_loss((1 - label_input.exp()) ** self.gamma * label_input, label_target, size_average=False)
+        bbox_loss = F.smooth_l1_loss(bbox_input[mask], bbox_target[mask], reduction='sum')
+        label_loss = F.nll_loss((1 - label_input.exp()) ** self.gamma * label_input, label_target, reduction='sum')
         loss = (bbox_loss + self.alpha * label_loss) / (num_positive + 1) / (1 + self.alpha)
         return loss

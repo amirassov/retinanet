5d14f8d9b584a081805c6e0f559d94cbfc1c845d
Vsevolod Poletaev <poletaev.va@gmail.com>
2018-10-09 19:24:41 +0300

    Make FPN backbone architecture configurable

--- fpnssd/models/fpn.py
+++ fpnssd/models/fpn.py
@@ -7,13 +7,22 @@ from torchvision.models import resnet
 
 def _upsample_add(x, y):
     _, _, h, w = y.size()
-    return F.upsample(x, size=(h, w), mode='bilinear', align_corners=False) + y
+    return F.interpolate(x, size=(h, w), mode='bilinear', align_corners=False) + y
+
+def get_channels(architecture):
+    if architecture in ['resnet18', 'resnet34']:
+        return [512, 256, 128, 64]
+    elif architecture in ['resnet50', 'resnet101', 'resnet152']:
+        return [2048, 1024, 512, 256]
+    else:
+        raise Exception(f'{architecture} is not supported as backbone')
 
 
 class ResNetFPN(nn.Module):
-    def __init__(self, pretrained=True):
+    def __init__(self, pretrained=True, architecture='resnet18'):
         super().__init__()
-        encoder = resnet.resnet18(pretrained)
+        encoder = getattr(resnet, architecture)(pretrained)
+        channles = get_channels(architecture)
 
         self.conv1 = nn.Sequential(encoder.conv1, encoder.bn1, encoder.relu, encoder.maxpool)
         self.conv2 = encoder.layer1
@@ -21,11 +30,11 @@ class ResNetFPN(nn.Module):
         self.conv4 = encoder.layer3
         self.conv5 = encoder.layer4
 
-        self.top_conv = nn.Conv2d(512, 256, kernel_size=1)
+        self.top_conv = nn.Conv2d(channles[0], 256, kernel_size=1)
 
-        self.lateral_conv1 = nn.Conv2d(256, 256, kernel_size=1)
-        self.lateral_conv2 = nn.Conv2d(128, 256, kernel_size=1)
-        self.lateral_conv3 = nn.Conv2d(64, 256, kernel_size=1)
+        self.lateral_conv1 = nn.Conv2d(channles[1], 256, kernel_size=1)
+        self.lateral_conv2 = nn.Conv2d(channles[2], 256, kernel_size=1)
+        self.lateral_conv3 = nn.Conv2d(channles[3], 256, kernel_size=1)
 
         self.smooth_conv1 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
         self.smooth_conv2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
